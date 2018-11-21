344682363e68b4bdd99b0d5b6ef63cb380c15369
Teddy <tmiguel.calderon@gmail.com>
2018-11-20 22:24:42 -0600

    funcion movimiento
    solo funcion movimiento enviando informacion por puerto serie para hacer pruebas con el sensor

--- sensor_de__movimiento/sensor_de__movimiento.ino
+++ sensor_de__movimiento/sensor_de__movimiento.ino
@@ -1,9 +1,40 @@
+/* le ponen la paja de quien lo escribio organicacion 
+bla bla bla bla  etc etc */
+
+
+const int sPIR = 8; // Sensor de movimiento PIR | Pin 8 - Arduino Uno;
+const int led = 13; // Led | Pin 13 - Arduino Uno;
+
+int mov = 0; // Variable que guarda el cambio de estado del PIR
+
+
 void setup() {
-  // put your setup code here, to run once:
+ // inicializamos variables y configuracion 
+Serial.begin(9600);
+pinMode(led,OUTPUT);
+pinMode(sPIR,INPUT);
+Serial.println("Prueba Sensor de Movimiento IR");
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+ 
+movimiento();
 }
+
+
+void movimiento(){
+  mov = digitalRead(sPIR);
+  delay(50);
+  if (mov == HIGH) {
+    Serial.print(mov); Serial.print(" : "); Serial.println("Movimiento detectado"); 
+    digitalWrite(led,HIGH);
+    delay(500);
+    digitalWrite(led,LOW);
+    mov = 0;
+  }
+  else {
+    Serial.print(mov); Serial.print(" : "); Serial.println("No se detecta movimiento");
+    delay(100);
+    }
+  }
